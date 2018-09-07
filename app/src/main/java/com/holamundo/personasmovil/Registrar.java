1995d07df0f1c0c31c997b1aaf514f792ef41ad9
Andres Tarra <afelipe0113@gmail.com>
2018-09-06 21:16:45 -0500

    Aplicacion terminada sin validaciones

--- app/src/main/java/com/holamundo/personasmovil/Registrar.java
+++ app/src/main/java/com/holamundo/personasmovil/Registrar.java
@@ -1,4 +1,41 @@
 package com.holamundo.personasmovil;
 
-public class Registrar {
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Registrar extends Activity{
+    private EditText txtCedula, txtNombre, txtApellido;
+    @Override
+    protected  void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar);
+        txtCedula = findViewById(R.id.txtCedula);
+        txtApellido = findViewById(R.id.txtApellido);
+        txtNombre = findViewById(R.id.txtNombre);
+    }
+
+    public void guardar(View v){
+        String ced, nomb, apell;
+        ced = txtCedula.getText().toString();
+        nomb = txtNombre.getText().toString();
+        apell = txtApellido.getText().toString();
+
+        Persona p = new Persona(ced, nomb, apell);
+        p.guardar();
+        Toast.makeText(this, getResources().getString(R.string.mensaje),Toast.LENGTH_SHORT).show();
+        borrar();
+    }
+
+    public void limpiar(View v){
+        borrar();
+    }
+
+    private void borrar(){
+        txtCedula.setText("");
+        txtNombre.setText("");
+        txtApellido.setText("");
+    }
 }
